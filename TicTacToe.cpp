7502544c652897edd6f9264418db3511e7373819
Umar Haroon <umar.m.haroon@gmail.com>
2021-09-23 11:23:58 -0600

    Added PlayerChoice

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,16 +1,58 @@
 #include <iostream>
 #include <vector>
-//Tic Tac Toe by Trevor Green and Umar Haroon 
-//Trevor is person 1 and Umar is person 2
 std::vector<std::vector<std::string> > CreateBoard ()
 {
    return std::vector<std::vector<std::string> > (3, std::vector<std::string>(3, ""));
 }
+void PlaceMarker(std::vector<std::vector<std::string> > &board, int input, std::string value) {
+switch (input)
+  {
+  case 1:
+    board[0][0] = value;
+    break;
 
-void GetPlayerChoice() {
+  case 2:
+    board[0][1] = value;
+    break;
+  case 3:
+    board[0][2] = value;
+    break;
+
+  case 4:
+    board[1][0] = value;
+    break;
+
+  case 5:
+    board[1][1] = value;
+    break;
+
+  case 6:
+    board[1][2] = value;
+    break;
+
+  case 7:
+    board[2][0] = value;
+    break;
+
+  case 8:
+    board[2][1] = value;
+    break;
+  case 9:
+    board[2][2] = value;
+    break;
+
+  default:
+    break;
+  }
+}
+void GetPlayerChoice(std::vector<std::vector<std::string> > &board) {
   int input;
   std::cout << "enter a value from 1-9. like a numpad, 1 is top left, 9 is bottom right" << std::endl;
   std::cin >> input;
+  std::string value;
+  std::cout << "Enter X or O: ";
+  std::cin >> value;
+  PlaceMarker(board, input, value);
 }
 
 void DisplayBoard(std::vector<std::vector<std::string> > *vec) {
@@ -21,7 +63,7 @@ void DisplayBoard(std::vector<std::vector<std::string> > *vec) {
     for (std::string col: row) {
       std::cout << " " << col;
       if (col_count < 2) {
-        std::cout << " | "; 
+        std::cout << " | ";
         col_count++;
       }
     }
@@ -37,6 +79,10 @@ int main()
 {
   std::vector<std::vector <std::string> > new_board=CreateBoard();
   // DisplayBoard(new_board);
-new_board[0][2] = "X";
-DisplayBoard(&new_board);
+
+  DisplayBoard(&new_board);
+  for (int i = 0; i < 9; i++) {
+  GetPlayerChoice(new_board);
+  DisplayBoard(&new_board);
+  }
 }
