fddc578a79b7897c6b2efb8a43dfe3820ef03363
Umar Haroon <umar.m.haroon@gmail.com>
2021-09-23 11:25:47 -0600

    Added PlayerChoice

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,47 +4,6 @@ std::vector<std::vector<std::string> > CreateBoard ()
 {
    return std::vector<std::vector<std::string> > (3, std::vector<std::string>(3, ""));
 }
-void PlaceMarker(std::vector<std::vector<std::string> > &board, int input, std::string value) {
-switch (input)
-  {
-  case 1:
-    board[0][0] = value;
-    break;
-
-  case 2:
-    board[0][1] = value;
-    break;
-  case 3:
-    board[0][2] = value;
-    break;
-
-  case 4:
-    board[1][0] = value;
-    break;
-
-  case 5:
-    board[1][1] = value;
-    break;
-
-  case 6:
-    board[1][2] = value;
-    break;
-
-  case 7:
-    board[2][0] = value;
-    break;
-
-  case 8:
-    board[2][1] = value;
-    break;
-  case 9:
-    board[2][2] = value;
-    break;
-
-  default:
-    break;
-  }
-}
 void GetPlayerChoice(std::vector<std::vector<std::string> > &board) {
   int input;
   std::cout << "enter a value from 1-9. like a numpad, 1 is top left, 9 is bottom right" << std::endl;
